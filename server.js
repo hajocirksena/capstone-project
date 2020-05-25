936c38f0db50818213d86375e180b11a1576426a
stqrsky <hajocirksena@gmail.com>
2020-05-25 08:26:42 +0200

    deploy via github

--- server.js
+++ server.js
@@ -12,39 +12,8 @@ server.use(express.static(path.join(__dirname, "client", "build")));
 
 server.use("/items", items);
 
-server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build"));
 });
 
-server.listen(port || 8050, () => console.log("server started"));
-
-// server.listen(...)
-
-// ... other imports
-// const path = require("path")
-
-// ... other app.use middleware
-// server.use(express.static(path.join(__dirname, "client", "build")))
-
-// ...
-// Right before your app.listen(), add this:
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
-
-// app.listen(...)
-
-// // ... other imports
-// const path = require("path")
-
-// // ... other app.use middleware
-// app.use(express.static(path.join(__dirname, "client", "build")))
-// server.use(express.static(path.join(__dirname, "client", "build")))
-
-// // ...
-// // Right before your app.listen(), add this:
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
-
-// app.listen(...)
+server.listen(port || 8050);
