eaba31e3cb14368c297ff3796d88f3e7f6ab8a6f
stqrsky <hajocirksena@gmail.com>
2020-05-28 19:49:37 +0200

    Add 2nd try

--- client/src/Items.js
+++ client/src/Items.js
@@ -47,7 +47,7 @@ export default function Items({ getItems }) {
               <DeleteButton id={item._id} onDelete={deleteCard} />
               {/* // THIS IS A PLACEHOLDER-LINK BECAUSE ITS IMPOSSIBLE TO SCAN QRCODES IN THE BROWSER // */}
               <QRFoundStyled>
-                <a href={'/' + item._id}>?</a>
+                <a href={'/items/' + item._id}>?</a>
               </QRFoundStyled>
             </CardStyled>
           </div>
