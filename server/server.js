d2d2ed06438ae0f68086c52ac8baa8ac79d2c8c8
stqrsky <hajocirksena@gmail.com>
2020-05-30 12:23:12 +0200

    Remove addressmethod from server

--- server/server.js
+++ server/server.js
@@ -20,6 +20,5 @@ server.get("*", (request, response) => {
 
 const { PORT = 3000, LOCAL_ADDRESS = "0.0.0.0" } = process.env;
 server.listen(PORT, LOCAL_ADDRESS, () => {
-  const address = server.address();
-  console.log("server listening at", address);
+  console.log("server listening at", PORT);
 });
