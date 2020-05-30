d7068e9e996cc0c04c54531efb5f9ece02bb1f24
stqrsky <hajocirksena@gmail.com>
2020-05-30 15:09:39 +0200

    changed path

--- server/server.js
+++ server/server.js
@@ -17,8 +17,15 @@ server.use("/items", items);
 // server.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../client/build.index.html"));
 // });
-server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client", "../client/build"));
+server.get("/", (req, res) => {
+  res.sendFile(
+    path.join(
+      __dirname,
+      "../client",
+      "../client/build",
+      "../client/build/index.html"
+    )
+  );
 });
 
 const PORT = process.env.PORT || 8050;
