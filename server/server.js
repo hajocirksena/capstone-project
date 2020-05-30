568abd7afc9058ed5d056a8a0803858061553fb4
stqrsky <hajocirksena@gmail.com>
2020-05-30 14:34:11 +0200

    changes route

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ server.use("/items", items);
 //   res.sendFile(path.join(__dirname, "../client/build.index.html"));
 // });
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "./client/build/index.html"));
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 8050;
