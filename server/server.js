d0e4acd366b2f4037b0ad5c3b85eb4146ae2adc0
stqrsky <hajocirksena@gmail.com>
2020-05-30 12:05:31 +0200

    Add port3000 to env

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import items from "./Items";
 
+const dotenv = require("dotenv");
+dotenv.config();
 const path = require("path");
 const server = express();
 const port = process.env.PORT || 8050;
-const dotenv = require("dotenv");
-dotenv.config();
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json({ extended: true }));
