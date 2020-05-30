28cf10e78db45fdf3a8efd55355b96a8543be446
stqrsky <hajocirksena@gmail.com>
2020-05-30 10:53:27 +0200

    Add cors

--- server/server.js
+++ server/server.js
@@ -1,32 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
 import items from "./Items";
+import cors from "cors";
 
 const path = require("path");
 const server = express();
 const port = process.env.PORT || 8050;
 const dotenv = require("dotenv");
-// require("dotenv").config();
 dotenv.config();
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json({ extended: true }));
 server.use(express.json());
+server.use(cors());
 server.use(express.static(path.join(__dirname, "client", "build")));
 
 server.use("/items", items);
 
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  server.use(express.static(path.join(__dirname, "client/build")));
-
-  server.get("*", (request, response) => {
-    response.sendFile(path.join(__dirname, "client/build/index.html"));
-  });
-}
-
-// server.get("*", (request, response) => {
-//   response.sendFile(path.join(__dirname, "client/build/index.html"));
-// });
+server.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
 server.listen(port || 8050);
