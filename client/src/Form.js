9f0db5e617dc7b5d3b099da4b0fc3a643d52651b
stqrsky <hajocirksena@gmail.com>
2020-06-01 11:52:20 +0200

    Add closebutton edit

--- client/src/Form.js
+++ client/src/Form.js
@@ -113,7 +113,7 @@ const FormStyled = styled.form`
 const CloseButtonStyled = styled.button`
   display: flex;
   justify-content: flex-end;
-  margin: 8px 16px;
+  margin: 8px 100px;
   border: none;
   background: var(--secondary);
 `
